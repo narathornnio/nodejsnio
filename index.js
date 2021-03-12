e01b95983d5aa47f6aa78c8fc312c2aef942f849
narathornnio <66000398+narathornnio@users.noreply.github.com>
2021-03-12 15:00:12 +0700

    Update index.js
    james

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let app = require('express')();
 
 let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+//let io = require('socket.io')(server);
 let WebSocket = require("ws");
 app.get("/", (req, res) => {
   const url = "ws://34.87.63.155:8080/feedService/productPrice";
