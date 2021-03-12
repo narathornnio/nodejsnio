874971176a494fcff3629fb80a6dc99c2d44d918
narathorn182359 <narathorn2539@hotmail.com>
2021-03-12 11:28:14 +0700

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let app = require('express')();
+/*let app = require('express')();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
  
@@ -35,3 +35,21 @@ var port = process.env.PORT || 3001;
 server.listen(port, function(){
    console.log('listening in http://localhost:' + port);
 });
+*/
+
+const express = require('express')
+const WebSocket = require('ws')
+const app = express()
+
+app.get('/', (req, res) => {
+    const url = 'ws://34.87.63.155:8080/feedService/productPrice'
+const connection = new WebSocket(url)
+connection.onmessage = e => {
+    res.send(e.data)
+  }
+  
+})
+
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
